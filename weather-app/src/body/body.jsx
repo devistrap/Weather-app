044af2c495fa912b64d9bf1811e6ef202483df2f
Bart Nieman <bart.nieman11@gmail.com>
2023-10-17 22:41:38 +0200

    got the api working not right way to read out all the data

--- weather-app/src/body/body.jsx
+++ weather-app/src/body/body.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import "./body.css"
 
-
-function body() {
+function Hero() {
+    function apiCall(){
+        fetch('https://api.weatherapi.com/v1/current.json?q=Amstelveen&key=6e0e7573a0a24bc0811195352231710')
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .then(data => {
+            document.getElementById('data').innerHTML  = data
+        })
+   }
     
     return (
-       
-       fetch('https://api.weatherapi.com/v1/current.json?q=Amstelveen&key=6e0e7573a0a24bc0811195352231710')
-       .then(response => response.json())
-       .then(data => console.log(data))
+        
+       <div className='body'>
+              <button onClick={apiCall}>Click me</button>
+              <p id='data'></p>
+       </div>
         
     )
 }
 
-export default body;
+export default Hero;
--- weather-app/src/main/main.jsx
+++ weather-app/src/main/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '../app/App.jsx'
 import Nav from '../nav/nav.jsx'
-import body from '../body/body.jsx'
+import Hero from '../body/body.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    
+    <Hero />
     <Nav />
+    
   </React.StrictMode>,
 )
